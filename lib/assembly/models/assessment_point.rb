230f8c184be732a501ebb8fc8a5632ce72dba146
NIcholas Kostelnik <nkostelnik@gmail.com>
2017-12-19 12:50:00 +0000

    Remove old method

--- lib/assembly/models/assessment_point.rb
+++ lib/assembly/models/assessment_point.rb
@@ -6,20 +6,6 @@ module Assembly
     def results(params={})
       pages(path + "#{rank}/results", params)
     end
-
-    private
-
-    def pages(path, params)
-      results = []
-      page = 1
-      while page
-        response = client.get(path, {page: page, per_page: 100}.merge(params))
-        ret = Util.build(response, client)
-        results += ret.data
-        page = ret.next_page
-      end
-      results
-    end
   end
 
   Resource.build(AssessmentPoint)
