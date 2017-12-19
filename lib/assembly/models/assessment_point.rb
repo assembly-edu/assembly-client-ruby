58c2c6a264dd0c71bf5ec69491971ebe83e55f11
NIcholas Kostelnik <nkostelnik@gmail.com>
2017-12-19 11:49:00 +0000

    Assessment Points results

--- lib/assembly/models/assessment_point.rb
+++ lib/assembly/models/assessment_point.rb
@@ -2,8 +2,22 @@ module Assembly
   class AssessmentPoint < ApiModel
     include Assembly::Actions::Read
     include Assembly::Actions::List
-    include Assembly::Actions::Create
-    include Assembly::Actions::Delete
+
+    def pages(path, params)
+      results = []
+      page = 1
+      while page
+        response = client.get(path, {page: page, per_page: 100}.merge(params))
+        ret = Util.build(response, client)
+        results += ret.data
+        page = ret.next_page
+      end
+      results
+    end
+
+    def results(params={})
+      pages(path + "#{rank}/results", params)
+    end
   end
 
   Resource.build(AssessmentPoint)
